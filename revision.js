46e0ca0b503c89b751b4f916b1da56e58fdcf8b1
Lucfontana <las.noches.luca@gmail.com>
2024-11-30 21:13:25 -0300

    changed style and added function to btnSi

--- revision.js
+++ revision.js
@@ -3,7 +3,8 @@ function revision(){
     let nombreMinuscula = nombre.toLowerCase();
 
     if (nombreMinuscula.includes("nor") || nombreMinuscula.includes("amo") && nombreMinuscula.length < 20) {
-        document.getElementById("mostrarNombre").innerHTML = nombre;
+        //La línea de abajo mostraba el nombre cuando todo estaba correcto
+        // document.getElementById("mostrarNombre").innerHTML = nombre;
         window.location.href="correcto.html";
         
     }
@@ -33,15 +34,55 @@ function esconderContenido(){
 }
 
 function botonNo(){
+    var ss = document.styleSheets[0];
     let botonNo = document.getElementsByClassName("no")[0];
 
     botonNo.onmouseover = () => {
-        botonNo.style.position = "absolute";
+        ss.cssRules[6].style.position = "absolute";
         botonNo.style.left = Math.random()*200 + "px";
         botonNo.style.top = Math.random()*200 + "px";
     }
 }
 
-function botonSi(){
-    
+let ejeX = 0;
+let ejeY = 0;
+let vecesApretado = 0;
+let texto = "Texto cambiante";
+document.getElementById("changingText").innerHTML = texto;
+
+let botonSi = document.getElementById("botonSi");
+
+botonSi.addEventListener("click", (event) => {
+    ejeX = ejeX + 1;
+    ejeY = ejeY + 1;
+
+    apretarBtnSi();
+    //El switch debe ir adentro del eventListener para que ejecute 
+    //Cada vez que el boton es presionado
+    switch(vecesApretado){
+        case 1: 
+            texto = "El texto cambió por primera vez";
+            break;
+        case 2: 
+            texto = "El texto cambió por segunda vez";
+            break;
+        case 3:
+            texto = "El texto cambió por tercera vez";
+            break;
+        case 4:
+            texto = "El texto cambió por cuarta vez";
+            break;
+        case 5:
+            texto = "El texto cambió por quinta vez";
+            break;
+        default:
+            window.location.href="index.html";
+            return;
+    }
+    console.log(vecesApretado);
+    document.getElementById("changingText").innerHTML = texto;
+});
+
+function apretarBtnSi(){
+    vecesApretado += 1;    
 }
